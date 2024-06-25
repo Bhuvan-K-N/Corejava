25a7de409d047e9806bdadeccefabbf276509f89
bhuva <bhuva@BhuvanKN>
2024-06-25 20:26:29 +0530

    second commit

--- b96corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ b96corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -1,5 +1,19 @@
 package com.tnsif.encapsulation;
 
 public class EncapsulationDemo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encapsulation ob=new Encapsulation();// object creation
+	//	ob.name="raju";// calling the variables
+		//ob.age=33;
+		//ob.serial_num=22;
+		//ob.show();
+		ob.setAge(22);
+		ob.setName("raju");
+		ob.setSerialNum(55);
+		System.out.println(ob);
+	}
 
 }
