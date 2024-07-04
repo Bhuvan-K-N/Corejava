322305227b2b7116bdd5cbc493930a7fc800aff7
bhuva <bhuva@BhuvanKN>
2024-07-04 22:18:38 +0530

    Constructor

---
+++ b96corejava/src/com/tnsif/constructor/ConstructorDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.constructor;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer ob = new Customer();  //Constructor called
+		ob.setId(12);
+		ob.setName("bhuvan");
+		
+		System.out.println(ob);
+	}
+
+}
+ 
