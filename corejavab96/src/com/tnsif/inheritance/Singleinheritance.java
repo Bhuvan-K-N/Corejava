68ef20d4fe5c763f84a8e7fd84b3dfabd3cf448b
bhuva <bhuva@BhuvanKN>
2024-07-19 11:33:00 +0530

    Inheritance Programs

---
+++ corejavab96/src/com/tnsif/inheritance/Singleinheritance.java
@@ -0,0 +1,12 @@
+package com.tnsif.inheritance;
+
+public class Singleinheritance {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Details of students: ");
+		Students ob = new Students("Bhuvan", 45, 58884844, 984498784,"Dbit", 21);
+		System.out.println(ob);
+	}
+
+
+}
