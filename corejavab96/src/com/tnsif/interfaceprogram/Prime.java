d14a530b7f2d8457173043c392ccc998743e729c
bhuva <bhuva@BhuvanKN>
2024-07-19 12:04:02 +0530

    InterfacePrograms

---
+++ corejavab96/src/com/tnsif/interfaceprogram/Prime.java
@@ -0,0 +1,17 @@
+package com.tnsif.interfaceprogram;
+
+public class Prime implements Amazon{
+
+	@Override
+	public void welcome() {
+		// TODO Auto-generated method stub
+		System.out.println("welcome Bhuvan a prime member");
+	}
+
+	@Override
+	public void thankyou() {
+		// TODO Auto-generated method stub
+		System.out.println("Thank you Bhuvan a prime member");
+	}
+
+}
