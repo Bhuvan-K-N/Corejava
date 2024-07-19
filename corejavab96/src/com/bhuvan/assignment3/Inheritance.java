f8de47887c3ae4ba9d81a28ce98ff890c09f469c
bhuva <bhuva@BhuvanKN>
2024-07-19 10:28:45 +0530

    Third Assignment

---
+++ corejavab96/src/com/bhuvan/assignment3/Inheritance.java
@@ -0,0 +1,12 @@
+package com.bhuvan.assignment3;
+
+public class Inheritance {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			State st=new State("Asia", "Mteverest", "Indian ocean", "India", "Mullayanagiri hills", "Cauvery", "Karnataka", "kannada");
+			System.out.println("MultipleInheritance");
+			System.out.println(st);
+		}
+	}
