38f09c0f7ad86210fbf99093496624d92175d10b
bhuva <bhuva@BhuvanKN>
2024-07-19 11:11:57 +0530

    second commit

---
+++ corejavab96/src/com/tnsif/encapsulationprogram/EncapsulationDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.encapsulationprogram;
+
+public class EncapsulationDemo  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob = new Encapsulation();
+//		ob.name="Bhuvan";
+//		ob.age=33;
+//		ob.serialNum=21;
+		
+		ob.setAge(21);
+		ob.setName("Bhuvan");
+		ob.setSerialNum(12);
+		System.out.println(ob);
+
+	}
+     
+}
