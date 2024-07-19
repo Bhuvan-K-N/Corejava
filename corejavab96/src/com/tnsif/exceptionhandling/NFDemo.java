3e225aa4812b7d3a3c7f91cf121dae52b44bb094
bhuva <bhuva@BhuvanKN>
2024-07-19 11:23:58 +0530

    Exception Handling Programs

---
+++ corejavab96/src/com/tnsif/exceptionhandling/NFDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.exceptionhandling;
+
+public class NFDemo {
+
+	private static final String NestedTry = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NestedTry.chars();
+	}
+
+}
